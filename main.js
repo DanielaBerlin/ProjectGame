cca1850b3a9d45df3bc0bcd6c2ba50821bd54e2f
Daniela <danielacardona2203@gmail.com>
2022-11-01 14:41:56 +0100

    update

--- main.js
+++ main.js
@@ -2,24 +2,29 @@
 let player = new Player()
 let obstacles = new Obstacles()
 let prize = new Prize()
+let secondprize = new Secondprize()
+
+const game = new Game()
 
 function preload () {
     player.preload()
     obstacles.preload()
     prize.preload()
+    secondprize.preload()
     
 }
 
 
 function setup() {
 	createCanvas(700, 900)
-    background(106, 92, 92)
 }
 
 
 function draw () {
+    clear()
+    background(106, 92, 92)
     player.draw()
     obstacles.draw()
     prize.draw()
-
+    secondprize.draw()
 }
--- player.js
+++ player.js
@@ -3,13 +3,13 @@ class Player {
         this.tony
         this.x = 250
         this.y = 750
-        this.espresso
     }
     preload() {
         this.tony = loadImage('./images/tony.png')
     }
     draw(){
         image(this.tony, this.x , this.y, 150, 150)
+ 
     }
 
 }
@@ -18,6 +18,8 @@ class Player {
 class Obstacles {
     constructor(){
         this.gun
+        this.x = 550
+        this.y = (Math.random() * 60) / 50
 
     }
 
@@ -25,26 +27,54 @@ class Obstacles {
         this.gun = loadImage('./images/pistola.png')
     }
     draw(){
-        image(this.gun, 550, 50, 60, 50)
-    }
 
+        image(this.gun, this.x, this.y, 60, 50)
+
+
+        this.x += 0
+        this.y += 1
+    }
 }
 
 class Prize {
     constructor(){
         this.jamon
-        this.espresso
+        this.x = 25
+        this.y = 50
+        this.y = (Math.random() * 60) / 50
 
     }
 
     preload(){
         this.jamon = loadImage('./images/jamon.png')
+    }
+    draw(){
+        image(this.jamon, this.x, this.y, 70, 60)
+       
+        this.x += 0
+        this.y += 1
+    
+    }
+
+}
+
+
+class Secondprize {
+    constructor(){
+        this.espresso
+        this.x = 250
+        this.y = (Math.random() * 60) / 50
+    }
+
+    preload(){
         this.espresso = loadImage('./images/espresso.png')
     }
     draw(){
-        image(this.jamon, 25, 50, 70, 60)
-        image(this.espresso, 250, 60, 80, 40)
-       
-    }
+        
+        image(this.espresso, this.x, this.y, 80, 40)
+  
+    this.x += 0
+    this.y += 1   
 
+    }
 }
